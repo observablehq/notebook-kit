1114cd6838386e9f6e0cc4a68162518b8d0a4c5a
Gordon Smith <GordonJSmith@gmail.com>
2025-08-09 17:25:32 +0100

    Allow instantiation  of multiple notebooks inside a single scope
    Signed-off-by: Gordon Smith <GordonJSmith@gmail.com>

--- src/runtime/index.ts
+++ src/runtime/index.ts
@@ -1,8 +1,3 @@
-import type {Module} from "@observablehq/runtime";
-import {Runtime} from "@observablehq/runtime";
-import {fileAttachments} from "./stdlib/fileAttachment.js";
-import {library} from "./stdlib/index.js";
-
 export * from "./define.js";
 export * from "./display.js";
 export * from "./inspect.js";
@@ -12,14 +7,3 @@ export type * from "./stdlib/databaseClient.js";
 export type * from "./stdlib/fileAttachment.js";
 export {DatabaseClient} from "./stdlib/databaseClient.js";
 export {FileAttachment, registerFile} from "./stdlib/fileAttachment.js";
-
-export const runtime = Object.assign(new Runtime({...library, __ojs_runtime: () => runtime}), {fileAttachments});
-export const main = (runtime as typeof runtime & {main: Module}).main = runtime.module();
-
-main.constructor.prototype.defines = function (this: Module, name: string): boolean {
-  return (
-    this._scope.has(name) ||
-    this._builtins.has(name) ||
-    this._runtime._builtin._scope.has(name)
-  );
-};
