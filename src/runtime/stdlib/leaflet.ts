6fc9c96f2361f0b6bf13dcf5ba5b7cfc97f04be6
Mike Bostock <mbostock@gmail.com>
2026-02-15 15:07:31 -0800

    await leaflet.css

--- src/runtime/stdlib/leaflet.ts
+++ src/runtime/stdlib/leaflet.ts
@@ -5,6 +5,12 @@ export * from "https://cdn.jsdelivr.net/npm/leaflet/+esm";
 Icon.Default.imagePath = "https://cdn.jsdelivr.net/npm/leaflet/dist/images/";
 
 const link = document.createElement("link");
-link.href = "https://cdn.jsdelivr.net/npm/leaflet/dist/leaflet.css";
 link.rel = "stylesheet";
+link.type = "text/css";
+link.href = "https://cdn.jsdelivr.net/npm/leaflet/dist/leaflet.css";
+const loaded = new Promise((resolve, reject) => {
+  link.onload = resolve;
+  link.onerror = reject;
+});
 document.head.appendChild(link);
+await loaded;
