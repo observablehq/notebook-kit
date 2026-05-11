18bf5e4b0a190cdc59e433fd44f4b1aae0fb3f0e
Mike Bostock <mbostock@gmail.com>
2026-05-11 06:41:21 -0700

    fix super call

--- src/runtime/stdlib/zip.ts
+++ src/runtime/stdlib/zip.ts
@@ -25,7 +25,7 @@ class ZipArchiveEntry extends AbstractFile {
   declare private readonly _: JSZip.JSZipObject;
   declare private _url: Promise<string>;
   constructor(object: JSZip.JSZipObject) {
-    super(undefined!, object.name);
+    super(object.name);
     Object.defineProperties(this, {
       _: {value: object},
       _url: {writable: true}
