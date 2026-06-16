20fbc638c4841721edc3ad4cfd3c90d4285bdd3e
Mike Bostock <mbostock@gmail.com>
2026-06-15 21:32:44 -0600

    even simpler

--- src/runtime/stdlib/dsv.test.ts
+++ src/runtime/stdlib/dsv.test.ts
@@ -1,141 +1,88 @@
 import {assert, describe, it} from "vitest";
-import type {Type} from "./dsv";
-import {coerceValue, enforceSchema, inferSchema} from "./dsv";
+import {inferTypes} from "./dsv";
+import {coerceBoolean, coerceDate, coerceNumber} from "./dsv";
 
-function inferType(values: string[]): Type {
-  return inferSchema(values.map((value) => ({value})))[0].type;
+function inferType(values: string[]): unknown[] {
+  return inferTypes(values.map((value) => ({value})), ["value"]).map(({value}) => value); // prettier-ignore
 }
 
-function enforceType(values: string[], type: Type): unknown[] {
-  return enforceSchema(values.map((value) => ({value})), [{name: "value", type}]).map(({value}) => value); // prettier-ignore
-}
-
-describe("inferSchema", () => {
-  it("infers the union of columns", () => {
-    assert.deepStrictEqual(inferSchema([{a: "2"}, {b: "bee"}, {c: "true"}]), [
-      {name: "a", type: "integer"},
-      {name: "b", type: "string"},
-      {name: "c", type: "boolean"}
-    ]);
-  });
-  it("infers integers", () => {
-    assert.deepStrictEqual(inferType(["2", "4", "6"]), "integer");
-  });
+describe("inferTypes", () => {
   it("infers numbers", () => {
-    assert.deepStrictEqual(inferType(["1.2", "3.4", "5.67"]), "number");
-  });
-  it("infers numbers with no leading zero", () => {
-    assert.deepStrictEqual(inferType([".2", ".4", ".67"]), "number");
+    assert.deepStrictEqual(inferType(["2", "4", "6"]), [2, 4, 6]);
+    assert.deepStrictEqual(inferType(["1.2", "3.4", "5.67"]), [1.2, 3.4, 5.67]);
+    assert.deepStrictEqual(inferType([".2", ".4", ".67"]), [0.2, 0.4, 0.67]);
+    assert.deepStrictEqual(inferType([0.1, 1, 2, 3, 9].map(String)), [0.1, 1, 2, 3, 9]);
+    assert.deepStrictEqual(inferType(["x", 1, 2, 3, 9, 1, 1, 1, 3, 2, 3].map(String)), [NaN, 1, 2, 3, 9, 1, 1, 1, 3, 2, 3]); // prettier-ignore
+    assert.deepStrictEqual(inferType([".0", ".1", "2", "3", "4", "5", "2", "3", ".9", "x"]), [0, 0.1, 2, 3, 4, 5, 2, 3, 0.9, NaN]); // prettier-ignore
   });
   it("infers booleans", () => {
-    assert.deepStrictEqual(inferType(["true", "", "false"]), "boolean");
+    assert.deepStrictEqual(inferType(["true", "", "false"]), [true, null, false]);
+    assert.deepStrictEqual(inferType([true, false, true, false, true, false, true, false, true, false, "pants on fire"].map(String)), [true, false, true, false, true, false, true, false, true, false, null]); // prettier-ignore
   });
   it("infers dates in common formats", () => {
-    assert.deepStrictEqual(inferType(["1/2/20", "2020-11-12 12:23:00", "", "2020-01-12"]), "date");
+    assert.deepStrictEqual(inferType(["1/2/20", "2020-11-12 12:23:00", "", "2020-01-12"]), [new Date("2020-01-02T07:00:00.000Z"), new Date("2020-11-12T19:23:00.000Z"), null, new Date("2020-01-12T00:00:00.000Z")]); // prettier-ignore
   });
   it("infers strings", () => {
-    assert.deepStrictEqual(inferType(["cat", "dog", "1,000", "null"]), "string");
-  });
-  it('considers "42n" to be a string, not a bigint', () => {
-    assert.deepStrictEqual(inferType(["10n", "22n", "0n"]), "string");
-  });
-  it("considers (exclusively) empty strings to be strings", () => {
-    assert.deepStrictEqual(inferType(["", ""]), "string");
-  });
-  it("considers mixed integers and numbers to be numbers, not integers", () => {
-    assert.deepStrictEqual(inferType([0.1, 1, 2, 3, 9, 10, 11, 12, 13].map(String)), "number");
-  });
-  it('considers "NaN" to be a string, not a number', () => {
-    assert.deepStrictEqual(inferType(["NaN", "NaN", "NaN", "1", "2", "3"]), "string");
-  });
-  it("infers mostly integers as integers", () => {
-    assert.deepStrictEqual(inferType(["x", 1, 2, 3, 9, 1, 1, 1, 3, 2, 3].map(String)), "integer");
-  });
-  it("infers partly integers as strings", () => {
-    assert.deepStrictEqual(inferType(["x", 1, 2, 3, 9, 1, 1, 3].map(String)), "string");
-  });
-  it("infers mostly numbers as numbers", () => {
-    assert.deepStrictEqual(inferType([".0", ".1", "2", "3", "4", "5", "2", "3", ".9", "x"]), "number"); // prettier-ignore
-  });
-  it("infers partly numbers as strings", () => {
-    assert.deepStrictEqual(inferType([".0", ".1", "2", "3", "4", "5", "2", "3", "x"]), "string");
-  });
-  it("infers mostly booleans as booleans", () => {
-    assert.deepStrictEqual(inferType([true, false, true, false, true, false, true, false, true, false, "pants on fire"].map(String)), "boolean"); // prettier-ignore
-  });
-  it("infers partly booleans as strings", () => {
-    assert.deepStrictEqual(inferType([true, false, true, false, true, false, "pants on fire"].map(String)), "string"); // prettier-ignore
+    assert.deepStrictEqual(inferType(["cat", "dog", "1,000", "null"]), ["cat", "dog", "1,000", "null"]); // prettier-ignore
+    assert.deepStrictEqual(inferType(["10n", "22n", "0n"]), ["10n", "22n", "0n"]);
+    assert.deepStrictEqual(inferType(["", ""]), ["", ""]);
+    assert.deepStrictEqual(inferType(["NaN", "NaN", "NaN", "1", "2", "3"]), ["NaN", "NaN", "NaN", "1", "2", "3"]); // prettier-ignore
+    assert.deepStrictEqual(inferType(["x", 1, 2, 3, 9, 1, 1, 3].map(String)), ["x", 1, 2, 3, 9, 1, 1, 3].map(String)); // prettier-ignore
+    assert.deepStrictEqual(inferType([".0", ".1", "2", "3", "4", "5", "2", "3", "x"]), [".0", ".1", "2", "3", "4", "5", "2", "3", "x"]); // prettier-ignore
+    assert.deepStrictEqual(inferType([true, false, true, false, true, false, "pants on fire"].map(String)), ["true", "false", "true", "false", "true", "false", "pants on fire"]); // prettier-ignore
   });
 });
 
-describe("enforceSchema", () => {
-  it("enforces integers", () => {
-    assert.deepStrictEqual(enforceType(["1", "2", "4", "3", ""], "integer"), [1, 2, 4, 3, NaN]);
-  });
-  it("enforces numbers", () => {
-    assert.deepStrictEqual(enforceType(["1.2", "4.3", "0.1", ""], "number"), [1.2, 4.3, 0.1, NaN]);
-  });
-  it("enforces booleans", () => {
-    assert.deepStrictEqual(enforceType(["true", "false", ""], "boolean"), [true, false, null]);
-  });
-  it("enforces dates", () => {
-    assert.deepStrictEqual(enforceType(["2002-01-01", "2003-01-01", ""], "date"), [new Date("2002-01-01"), new Date("2003-01-01"), null]); // prettier-ignore
-  });
-});
-
-describe("coerceValue", () => {
-  it("coerces to integer", () => {
-    assert.deepStrictEqual(coerceValue("1.2", "integer"), 1.2);
-    assert.deepStrictEqual(coerceValue(" 1.2", "integer"), 1.2);
-    assert.deepStrictEqual(coerceValue(" 1.2 ", "integer"), 1.2);
-    assert.deepStrictEqual(coerceValue("10", "integer"), 10);
-    assert.deepStrictEqual(coerceValue("0", "integer"), 0);
-    assert.deepStrictEqual(coerceValue("A", "integer"), NaN);
-    assert.deepStrictEqual(coerceValue("", "integer"), NaN);
-    assert.deepStrictEqual(coerceValue(" ", "integer"), NaN);
-    assert.deepStrictEqual(coerceValue("null", "integer"), NaN);
-  });
+describe("coerceNumber", () => {
   it("coerces to number", () => {
-    assert.deepStrictEqual(coerceValue("1.2", "number"), 1.2);
-    assert.deepStrictEqual(coerceValue(" 1.2", "number"), 1.2);
-    assert.deepStrictEqual(coerceValue(" 1.2 ", "number"), 1.2);
-    assert.deepStrictEqual(coerceValue("0", "number"), 0);
-    assert.deepStrictEqual(coerceValue("", "number"), NaN);
-    assert.deepStrictEqual(coerceValue(" ", "number"), NaN);
-    assert.deepStrictEqual(coerceValue("A", "number"), NaN);
-    assert.deepStrictEqual(coerceValue("null", "number"), NaN);
-    assert.deepStrictEqual(coerceValue("undefined", "number"), NaN);
-    assert.deepStrictEqual(coerceValue("{a: 1}", "number"), NaN);
+    assert.deepStrictEqual(coerceNumber("1.2"), 1.2);
+    assert.deepStrictEqual(coerceNumber(" 1.2"), 1.2);
+    assert.deepStrictEqual(coerceNumber(" 1.2 "), 1.2);
+    assert.deepStrictEqual(coerceNumber("10"), 10);
+    assert.deepStrictEqual(coerceNumber("0"), 0);
+    assert.deepStrictEqual(coerceNumber("A"), NaN);
+    assert.deepStrictEqual(coerceNumber(""), NaN);
+    assert.deepStrictEqual(coerceNumber(" "), NaN);
+    assert.deepStrictEqual(coerceNumber("null"), NaN);
+    assert.deepStrictEqual(coerceNumber("1.2"), 1.2);
+    assert.deepStrictEqual(coerceNumber(" 1.2"), 1.2);
+    assert.deepStrictEqual(coerceNumber(" 1.2 "), 1.2);
+    assert.deepStrictEqual(coerceNumber("0"), 0);
+    assert.deepStrictEqual(coerceNumber(""), NaN);
+    assert.deepStrictEqual(coerceNumber(" "), NaN);
+    assert.deepStrictEqual(coerceNumber("A"), NaN);
+    assert.deepStrictEqual(coerceNumber("null"), NaN);
+    assert.deepStrictEqual(coerceNumber("undefined"), NaN);
+    assert.deepStrictEqual(coerceNumber("{a: 1}"), NaN);
   });
+});
+
+describe("coerceBoolean", () => {
   it("coerces to boolean", () => {
-    assert.deepStrictEqual(coerceValue("true", "boolean"), true);
-    assert.deepStrictEqual(coerceValue("True   ", "boolean"), true);
-    assert.deepStrictEqual(coerceValue("False", "boolean"), false);
-    assert.deepStrictEqual(coerceValue("false", "boolean"), false);
-    assert.deepStrictEqual(coerceValue("1", "boolean"), null); // TODO null vs. undefined?
-    assert.deepStrictEqual(coerceValue("2", "boolean"), null);
-    assert.deepStrictEqual(coerceValue("0", "boolean"), null);
-    assert.deepStrictEqual(coerceValue("{}", "boolean"), null);
-    assert.deepStrictEqual(coerceValue("null", "boolean"), null);
-    assert.deepStrictEqual(coerceValue("undefined", "boolean"), null);
+    assert.deepStrictEqual(coerceBoolean("true"), true);
+    assert.deepStrictEqual(coerceBoolean("True   "), true);
+    assert.deepStrictEqual(coerceBoolean("False"), false);
+    assert.deepStrictEqual(coerceBoolean("false"), false);
+    assert.deepStrictEqual(coerceBoolean("1"), null); // TODO null vs. undefined?
+    assert.deepStrictEqual(coerceBoolean("2"), null);
+    assert.deepStrictEqual(coerceBoolean("0"), null);
+    assert.deepStrictEqual(coerceBoolean("{}"), null);
+    assert.deepStrictEqual(coerceBoolean("null"), null);
+    assert.deepStrictEqual(coerceBoolean("undefined"), null);
   });
+});
+
+describe("coerceDate", () => {
   it("coerces to date", () => {
     const invalidDate = new Date(NaN);
-    assert.deepStrictEqual(coerceValue("12/12/2020", "date"), new Date("12/12/2020"));
-    assert.deepStrictEqual(coerceValue("12/12/2020  ", "date"), new Date("12/12/2020"));
-    assert.deepStrictEqual(coerceValue("2022-01-01T12:34:00Z", "date"), new Date("2022-01-01T12:34:00Z")); // prettier-ignore
-    assert.deepStrictEqual(coerceValue("{a: 1}", "date"), invalidDate);
-    assert.deepStrictEqual(coerceValue("true", "date"), invalidDate);
-    assert.deepStrictEqual(coerceValue("2020-1-12", "date"), invalidDate);
-    assert.deepStrictEqual(coerceValue("1675356739000", "date"), invalidDate);
-    assert.deepStrictEqual(coerceValue("undefined", "date"), invalidDate);
-    assert.deepStrictEqual(coerceValue("", "date"), null);
-  });
-  it("coerces to string", () => {
-    assert.deepStrictEqual(coerceValue("true", "string"), "true");
-    assert.deepStrictEqual(coerceValue("false", "string"), "false");
-    assert.deepStrictEqual(coerceValue("10", "string"), "10");
-    assert.deepStrictEqual(coerceValue("", "string"), "");
-    assert.deepStrictEqual(coerceValue(" ", "string"), " ");
+    assert.deepStrictEqual(coerceDate("12/12/2020"), new Date("12/12/2020"));
+    assert.deepStrictEqual(coerceDate("12/12/2020  "), new Date("12/12/2020"));
+    assert.deepStrictEqual(coerceDate("2022-01-01T12:34:00Z"), new Date("2022-01-01T12:34:00Z")); // prettier-ignore
+    assert.deepStrictEqual(coerceDate("{a: 1}"), invalidDate);
+    assert.deepStrictEqual(coerceDate("true"), invalidDate);
+    assert.deepStrictEqual(coerceDate("2020-1-12"), invalidDate);
+    assert.deepStrictEqual(coerceDate("1675356739000"), invalidDate);
+    assert.deepStrictEqual(coerceDate("undefined"), invalidDate);
+    assert.deepStrictEqual(coerceDate(""), null);
   });
 });
--- src/runtime/stdlib/dsv.ts
+++ src/runtime/stdlib/dsv.ts
@@ -1,106 +1,60 @@
 /* eslint-disable @typescript-eslint/no-unused-expressions */
 
-export type Type = "boolean" | "integer" | "number" | "date" | "string";
-
-export interface ColumnSchema {
-  /** The name of the column. */
-  name: string;
-  /** The type of the column. */
-  type: Type;
-}
-
 /** Accepts dates in the form of ISOString and LocaleDateString, with or without time. */
 const DATE_TEST = /^(([-+]\d{2})?\d{4}(-\d{2}(-\d{2}))|(\d{1,2})\/(\d{1,2})\/(\d{2,4}))([T ]\d{2}:\d{2}(:\d{2}(\.\d{3})?)?(Z|[-+]\d{2}:\d{2})?)?$/; // prettier-ignore
 
 /** The maximum number of rows to sample (including missing values). */
 const SAMPLE_SIZE = 100;
 
-export function inferSchema(
-  rows: Record<string, string>[],
-  columns = getAllKeys(rows)
-): ColumnSchema[] {
-  const schema: ColumnSchema[] = [];
-  const n = Math.min(rows.length, SAMPLE_SIZE);
+export function inferTypes<T extends Record<string, string>>(
+  rows: T[],
+  columns: (keyof T)[]
+): Record<keyof T, unknown>[] {
+  const output = rows as Record<keyof T, unknown>[];
+  const n = rows.length;
+  const k = Math.min(n, SAMPLE_SIZE);
   for (const column of columns) {
     let booleans = 0;
-    let integers = 0;
     let numbers = 0;
     let dates = 0;
     let strings = 0;
 
-    for (let i = 0; i < n; ++i) {
+    for (let i = 0; i < k; ++i) {
       const value = rows[i][column]?.trim();
       if (!value) continue;
       ++strings;
-      if (/^(true|false)$/i.test(value)) {
-        ++booleans;
-      } else if (!isNaN(Number(value))) {
-        ++numbers;
-        if (Number.isInteger(+value)) ++integers;
-      } else if (DATE_TEST.test(value)) {
-        ++dates;
-      }
+      if (/^(true|false)$/i.test(value)) ++booleans;
+      else if (!isNaN(Number(value))) ++numbers;
+      else if (DATE_TEST.test(value)) ++dates;
     }
 
     // Chose the non-string type with the greatest count that is also ≥90%; or
-    // if no such type meets that criterion, use string. Note: integer is more
-    // specific than number, and hence should be tested before number.
-    let type: Type = "string";
+    // if no such type meets that criterion, use string.
+    let coerce: ((value: string) => unknown) | undefined = undefined;
     let typeCount = Math.max(1, Math.ceil(strings * 0.9)) - 1;
-    if (booleans > typeCount) ((type = "boolean"), (typeCount = booleans));
-    if (integers > typeCount) ((type = "integer"), (typeCount = integers));
-    if (numbers > typeCount) ((type = "number"), (typeCount = numbers));
-    if (dates > typeCount) ((type = "date"), (typeCount = dates));
-    schema.push({name: column, type});
+    if (booleans > typeCount) ((coerce = coerceBoolean), (typeCount = booleans));
+    if (numbers > typeCount) ((coerce = coerceNumber), (typeCount = numbers));
+    if (dates > typeCount) ((coerce = coerceDate), (typeCount = dates));
+    if (!coerce) continue;
+
+    for (let i = 0; i < n; ++i) {
+      output[i][column] = coerce(rows[i][column]);
+    }
   }
-  return schema;
-}
-
-export function enforceSchema(
-  rows: Record<string, string>[],
-  schema: ColumnSchema[]
-): Record<string, unknown>[] & {schema: typeof schema} {
-  return Object.assign(rows.map((row) => coerceRow(row, schema)), {schema}); // prettier-ignore
-}
-
-export function coerceRow(
-  input: Record<string, string>,
-  schema: ColumnSchema[]
-): Record<string, unknown> {
-  const output: Record<string, unknown> = {};
-  for (const {name, type} of schema) output[name] = coerceValue(input[name], type);
   return output;
 }
 
-export function coerceValue(value: string, type: Type): unknown {
-  switch (type) {
-    case "boolean": {
-      const trimmed = value.trim().toLowerCase();
-      return trimmed === "true" ? true : trimmed === "false" ? false : null;
-    }
-    case "integer":
-    case "number": {
-      const trimmed = value.trim();
-      return trimmed ? Number(value) : NaN;
-    }
-    case "date": {
-      const trimmed = value.trim();
-      return trimmed ? new Date(DATE_TEST.test(trimmed) ? trimmed : NaN) : null;
-    }
-    default: {
-      return value;
-    }
-  }
+export function coerceBoolean(value: string): boolean | null {
+  const trimmed = value.trim().toLowerCase();
+  return trimmed === "true" ? true : trimmed === "false" ? false : null;
 }
 
-export function getAllKeys(rows: Record<string, string>[]): string[] {
-  const keys = new Set<string>();
-  for (const row of rows) {
-    if (row != null) {
-      for (const key of Object.keys(row)) {
-        keys.add(key);
-      }
-    }
-  }
-  return Array.from(keys);
+export function coerceNumber(value: string): number {
+  const trimmed = value.trim();
+  return trimmed ? Number(value) : NaN;
+}
+
+export function coerceDate(value: string): Date | null {
+  const trimmed = value.trim();
+  return trimmed ? new Date(DATE_TEST.test(trimmed) ? trimmed : NaN) : null;
 }
